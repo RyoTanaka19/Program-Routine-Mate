bcdf4f67c929927fa900e2f7f45df9e033b3fb04
ryo88 <ryo98tana@gmail.com>
2025-02-02 03:53:40 +0900

    add: 投稿機能の作成

--- app/controllers/study_logs_controller.rb
+++ app/controllers/study_logs_controller.rb
@@ -1,7 +1,25 @@
 class StudyLogsController < ApplicationController
-  def index
-  end
 
   def new
+    @study_log = StudyLog.new
   end
+
+  def create
+    @study_log = StudyLog.new(study_log_params)
+    if @study_log.save
+      redirect_to study_logs_path
+    else
+      render :new
+    end
+  end
+
+    def index
+      @study_logs = StudyLog.all
+    end
+
+    private
+
+    def study_log_params
+      params.require(:study_log).permit(:content, :hour, :text)
+    end
 end
---
+++ db/migrate/20250201094803_create_study_logs.rb
@@ -0,0 +1,11 @@
+class CreateStudyLogs < ActiveRecord::Migration[7.2]
+  def change
+    create_table :study_logs do |t|
+      t.string :content, null: false
+      t.integer :hour, null: false
+      t.text :text, null: false
+      
+      t.timestamps
+    end
+  end
+end
