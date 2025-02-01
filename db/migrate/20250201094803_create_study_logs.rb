abfccefd10cfa25e6ce5346beb212c099f031a29
ryo88 <ryo98tana@gmail.com>
2025-02-02 07:45:23 +0900

    テーブルが重複するというエラーの新しい解消

--- db/migrate/20250201094803_create_study_logs.rb
+++ db/migrate/20250201094803_create_study_logs.rb
@@ -1,6 +1,6 @@
 class CreateStudyLogs < ActiveRecord::Migration[7.2]
   def change
-    create_table :study_logs, if_not_exists: true do |t|
+    create_table :study_logs do |t|
       t.string :content, null: false
       t.integer :hour, null: false
       t.text :text, null: false
