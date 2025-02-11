051bc906e58e416ac585b996aff6b0f76ac2e5e6
ryo88 <ryo98tana@gmail.com>
2025-02-12 06:04:28 +0900

    modified: デプロイのための修正

--- app/controllers/study_logs_controller.rb
+++ app/controllers/study_logs_controller.rb
@@ -6,7 +6,7 @@ class StudyLogsController < ApplicationController
   end
 
   def create
-    @study_log = current_user.study_logs.build(study_log_params)
+    @study_log = current_user.study_logs.new(study_log_params)
     if @study_log.save
       redirect_to study_logs_path
     else
--- app/models/study_log.rb
+++ app/models/study_log.rb
@@ -1,5 +1,5 @@
 class StudyLog < ApplicationRecord
-  mount_uploader :image, StudyLogsImageUploader
   belongs_to :user
+  mount_uploader :image, StudyLogsImageUploader
   has_many :learning_comments, dependent: :destroy
 end
