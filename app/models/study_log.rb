e8a7520a4005be30538c38fe322ccfc2be2670cd
ryo88 <ryo98tana@gmail.com>
2025-02-19 12:43:52 +0900

    add:バリデーションの追加

--- app/models/study_log.rb
+++ app/models/study_log.rb
@@ -2,4 +2,11 @@ class StudyLog < ApplicationRecord
   belongs_to :user
   mount_uploader :image, StudyLogsImageUploader
   has_many :learning_comments, dependent: :destroy
+
+  # 学習内容 空なし
+  validates :content, presence: true
+  # 学習時間 空なし
+  validates :hour, presence: true
+  # 学んだこと 空なし
+  validates :text, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,10 +2,22 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable
 
   has_many :study_logs, dependent: :destroy
   has_many :learning_comments, dependent: :destroy
+
+  # 名前 空はなし、一意性
+  validates :name, presence: true, uniqueness: true
+  # 名前 メールアドレス 空はなし、一意性
+  validates :email, presence: true, uniqueness: true
+  # パスワード 空はなし
+  validates :password, presence: true
+  # パスワード確認 空はなし
+  validates :password_confirmation, presence: true
+  # パスワードとパスワード確認が一致するかどうか確認
+  validates :password, confirmation: { message: "が一致しません" }
+
 
   def own?(object)
     id == object&.user_id
