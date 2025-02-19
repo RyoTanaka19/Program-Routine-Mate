82b5a928a21c4ca498c438e959cd10ca0800b216
ryo88 <ryo98tana@gmail.com>
2025-02-20 06:27:59 +0900

    デプロイ4

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -1,9 +1,10 @@
-# bin/render-build.sh内
-  #!/usr/bin/env bash
-  # exit on error
-  set -o errexit
-  
-  bundle install
-  bundle exec rake assets:precompile
-  bundle exec rake assets:clean
-  bundle exec rake db:migrate 
+#!/usr/bin/env bash
+# exit on error
+set -o errexit
+
+export RAILS_ENV=production  # 明示的に設定
+bundle install
+bundle exec rake assets:precompile
+bundle exec rake assets:clean
+# bundle exec rake db:migrate
+DISABLE_DATABASE_ENVIRONMENT_CHECK=1 bundle exec rake db:migrate:reset
