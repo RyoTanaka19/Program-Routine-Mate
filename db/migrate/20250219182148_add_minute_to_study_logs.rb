fd85dbf8b10d9751fbd8c46b541ebd6199076312
ryo88 <ryo98tana@gmail.com>
2025-02-20 07:07:05 +0900

    デプロイ5

--- db/migrate/20250219182148_add_minute_to_study_logs.rb
+++ db/migrate/20250219182148_add_minute_to_study_logs.rb
@@ -1,5 +1,5 @@
 class AddMinuteToStudyLogs < ActiveRecord::Migration[7.2]
   def change
-    add_column :study_logs, :minute, :integer, null: false
+    add_column :study_logs, :minute, :integer, null:false
   end
 end
