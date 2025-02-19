cc4cfb18d246c24acb334349ab11b82902fbd8cf
ryo88 <ryo98tana@gmail.com>
2025-02-20 05:17:26 +0900

    modified:デプロイ

--- db/migrate/20250219182440_add_second_to_study_logs.rb
+++ db/migrate/20250219182440_add_second_to_study_logs.rb
@@ -1,5 +1,5 @@
 class AddSecondToStudyLogs < ActiveRecord::Migration[7.2]
   def change
-    add_column :study_logs, :second, :integer, null: false
+    add_column :study_logs, :second, :integer, null: false, default: 0
   end
 end
