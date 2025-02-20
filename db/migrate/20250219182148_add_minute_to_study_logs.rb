9a68b39c21de19287b872c200eec6755512def09
ryo88 <ryo98tana@gmail.com>
2025-02-20 09:57:54 +0900

    modified: リントチェック修正

--- db/migrate/20250219182148_add_minute_to_study_logs.rb
+++ db/migrate/20250219182148_add_minute_to_study_logs.rb
@@ -1,5 +1,5 @@
 class AddMinuteToStudyLogs < ActiveRecord::Migration[7.2]
   def change
-    add_column :study_logs, :minute, :integer, null:false, default: 0
+    add_column :study_logs, :minute, :integer, null: false, default: 0
   end
 end
