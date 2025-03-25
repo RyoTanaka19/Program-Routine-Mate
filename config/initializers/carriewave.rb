cecbdc3b0e4db65da5b6ef8e53399846ee0a85e9
ryo88 <ryo98tana@gmail.com>
2025-03-25 21:05:31 +0900

    modified:リントチェツク

--- config/initializers/carriewave.rb
+++ config/initializers/carriewave.rb
@@ -11,7 +11,7 @@ CarrierWave.configure do |config|
       provider: "AWS",
       aws_access_key_id: ENV["AWS_ACCESS_KEY_ID"], # 環境変数
       aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"], # 環境変数
-      region:  ENV["AWS_REGION"],
+      region: ENV["AWS_REGION"],
       path_style: true
     }
 end
