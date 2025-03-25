f9914050e0dbd6a804ad41e16be4ec592476add4
ryo88 <ryo98tana@gmail.com>
2025-03-25 21:20:51 +0900

    modifued:修正

--- config/initializers/carriewave.rb
+++ config/initializers/carriewave.rb
@@ -4,12 +4,12 @@ require "carrierwave/storage/fog"
 
 CarrierWave.configure do |config|
     config.storage :fog
-    config.fog_directory  = ENV["AWS_BUCKET_NAME"] # 作成したバケット名を記述
+    config.fog_directory = ENV["AWS_BUCKET_NAME"]
     config.fog_public = false
     config.fog_credentials = {
       provider: "AWS",
-      aws_access_key_id: ENV["AWS_ACCESS_KEY_ID"], # 環境変数
-      aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"], # 環境変数
+      aws_access_key_id: ENV["AWS_ACCESS_KEY_ID"],
+      aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],
       region: ENV["AWS_REGION"],
       path_style: true
     }
