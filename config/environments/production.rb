55e5daefdb11463b384673df66a67c3b0dfcc921
ryo88 <ryo98tana@gmail.com>
2025-04-16 16:55:00 +0900

    modified:ドメイン名反映

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -5,6 +5,8 @@ Rails.application.configure do
 
   # Code is not reloaded between requests.
   config.enable_reloading = false
+  config.hosts << "program-routine-mate.onrender.com"      # 独自ドメイン 
+  config.hosts << "www.program-routine-mate.onrender.com"  # サブドメイン
 
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
