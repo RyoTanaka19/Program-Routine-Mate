eddff4b739aa3d3d1f18d6764b4efd2325939291
ryo88 <ryo98tana@gmail.com>
2025-04-17 08:33:41 +0900

    modified: 修正

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -10,8 +10,5 @@ application.register('autocomplete', AutocompleteController);
 import HelloController from './hello_controller';
 application.register('hello', HelloController);
 
-import LoadingController from './loading_controller';
-application.register('loading', LoadingController);
-
 import RedirectController from './redirect_controller';
 application.register('redirect', RedirectController);
