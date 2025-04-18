637187516d49c1400ee3441fbc2614f494502405
ryo88 <ryo98tana@gmail.com>
2025-04-18 22:53:51 +0900

    modified;

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -13,22 +13,24 @@ module ApplicationHelper
       reverse: true,
       charset: "utf-8",
       description: "ProgramRoutineMateは、プログラミング学習を継続的にサポートするアプリです。ユーザーが学習を記録し、モチベーションを維持するためのツールを提供します。",
-      canonical: request.original_url,
+      keywords: "プログラミング","学習","習慣",
+      canonical: "https://program-routine-mate.com",
       separator: "|",
       og: {
         site_name: :site,
         title: :title,
         description: :description,
         type: "website",
-        url: request.original_url,
-        image: image_url("Program_Routine_Mate.png"),
+        url: https://program-routine-mate.com,
+        image: image_url("ogp.png"),
         local: "ja-JP"
       },
       twitter: {
         card: "summary_large_image",
-        site: "@https://x.com/58a_tanakaryo",  # ユーザー名の形式に修正
-        image: image_url("Program_Routine_Mate.png")
+        site: "@58a_tanakaryo",  # ユーザー名の形式に修正
+        image: image_url("ogp.png")
       }
     }
   end
 end
+
