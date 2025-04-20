8e3db784a0e75d0cb5d1a25e434c6bfbc9012fb6
ryo88 <ryo98tana@gmail.com>
2025-04-20 13:28:49 +0900

    modified:修正

--- db/migrate/20250312014220_create_user_study_badges.rb
+++ db/migrate/20250312014220_create_user_study_badges.rb
@@ -1,8 +1,9 @@
-class CreateUserStudyGenres < ActiveRecord::Migration[6.0]
+class CreateUserStudyBadges < ActiveRecord::Migration[7.2]
   def change
-    create_table :user_study_genres do |t|
-      t.integer :user_id
-      t.integer :study_genre_id
+    create_table :user_study_badges do |t|
+      t.references :user, null: false, foreign_key: true
+      t.references :study_badge, null: false, foreign_key: true
+      t.datetime :earned_at
 
       t.timestamps
     end
