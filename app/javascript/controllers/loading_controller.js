9b5a8a558d3279cd1051bfaa5c2a4883d22f4caa
ryo88 <ryo98tana@gmail.com>
2025-05-03 16:25:08 +0900

    modified:本リリース前1

--- app/javascript/controllers/loading_controller.js
+++ app/javascript/controllers/loading_controller.js
@@ -1,13 +1,17 @@
 import { Controller } from '@hotwired/stimulus';
 
+// Controller for showing loading spinner during form submission
 export default class extends Controller {
   static targets = ['spinner'];
+
   connect() {
     this.hide();
   }
+
   show() {
     this.spinnerTarget.classList.remove('hidden');
   }
+
   hide() {
     this.spinnerTarget.classList.add('hidden');
   }
