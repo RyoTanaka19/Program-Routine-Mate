c6af8ef91a69b4d3eacf59dd225eede29f225662
ryo88 <ryo98tana@gmail.com>
2025-05-05 18:34:13 +0900

    modified:リントチェっくの修正

--- app/controllers/users/passwords_controller.rb
+++ app/controllers/users/passwords_controller.rb
@@ -9,7 +9,7 @@ class Users::PasswordsController < Devise::PasswordsController
   # POST /resource/password
   def create
     self.resource = resource_class.send_reset_password_instructions(resource_params)
-  
+
     if successfully_sent?(resource)
       redirect_to after_sending_reset_password_instructions_path_for(resource_name)
     else
