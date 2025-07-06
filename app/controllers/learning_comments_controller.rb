f7ff75dce9674cf294b8a3063367d2fed939d0be
ryo88 <ryo98tana@gmail.com>
2025-07-07 02:37:36 +0900

    modified:フラッシュメッセージ

--- app/controllers/learning_comments_controller.rb
+++ app/controllers/learning_comments_controller.rb
@@ -1,68 +1,94 @@
 class LearningCommentsController < ApplicationController
   before_action :authenticate_user!, only: [ :create, :destroy, :edit, :update ]
 
-  def create
-    @study_log = StudyLog.find(params[:study_log_id])
-    @learning_comment = current_user.learning_comments.build(learning_comment_params)
-    @learning_comment.study_log = @study_log
+def create
+  @study_log = StudyLog.find(params[:study_log_id])
+  @learning_comment = current_user.learning_comments.build(learning_comment_params)
+  @learning_comment.study_log = @study_log
 
-    if @learning_comment.save
+  if @learning_comment.save
+    # コメントが保存できたらフラッシュメッセージを表示
+    flash[:notice] = "コメントを投稿しました"
 
-      if @learning_comment.user != @study_log.user
-
-       NotifyLineJob.perform_later(
-         nil, nil, nil, @study_log.user.id, @learning_comment.id
-        )
-      end
-
-      render turbo_stream: [
-        turbo_stream.replace(
-          "learning_comments-form",
-          partial: "learning_comments/form",
-          locals: { learning_comment: LearningComment.new, study_log: @study_log }
-        ),
-        turbo_stream.prepend(
-          "learning_comments-list",
-          partial: "learning_comments/learning_comment",
-          locals: { learning_comment: @learning_comment }
-        )
-      ]
-    else
-      render turbo_stream: turbo_stream.replace(
+    render turbo_stream: [
+      turbo_stream.replace(
         "learning_comments-form",
         partial: "learning_comments/form",
-        locals: { learning_comment: @learning_comment, study_log: @study_log }
+        locals: { learning_comment: LearningComment.new, study_log: @study_log }
+      ),
+      turbo_stream.prepend(
+        "learning_comments-list",
+        partial: "learning_comments/learning_comment",
+        locals: { learning_comment: @learning_comment }
+      ),
+      turbo_stream.replace(
+        "flash_messages",
+        partial: "shared/flash_messages" # フラッシュメッセージを更新
       )
-    end
+    ]
+  else
+    render turbo_stream: turbo_stream.replace(
+      "learning_comments-form",
+      partial: "learning_comments/form",
+      locals: { learning_comment: @learning_comment, study_log: @study_log }
+    )
   end
+end
 
-  def destroy
-    @learning_comment = current_user.learning_comments.find(params[:id])
 
-    @learning_comment.destroy!
+
+def destroy
+  @learning_comment = current_user.learning_comments.find(params[:id])
+
+  if @learning_comment.destroy
+    flash[:notice] = "コメントを削除しました"
+
+    render turbo_stream: [
+      turbo_stream.remove("learning_comment_#{@learning_comment.id}"),
+      turbo_stream.replace(
+        "flash_messages",
+        partial: "shared/flash_messages" # フラッシュメッセージを更新
+      )
+    ]
+  else
+    flash[:alert] = "コメントの削除に失敗しました"
+
+    render turbo_stream: turbo_stream.replace(
+      "flash_messages",
+      partial: "shared/flash_messages" # フラッシュメッセージを更新
+    )
   end
+end
 
   def edit
     @learning_comment = LearningComment.find(params[:id])
   end
 
-  def update
-    @learning_comment = LearningComment.find(params[:id])
+ def update
+  @learning_comment = LearningComment.find(params[:id])
 
-    if @learning_comment.update(learning_comment_params)
-      render turbo_stream: turbo_stream.replace(
+  if @learning_comment.update(learning_comment_params)
+    flash[:notice] = "コメントを編集しました"
+
+    render turbo_stream: [
+      turbo_stream.replace(
         "learning_comment_#{@learning_comment.id}",
         partial: "learning_comments/learning_comment",
         locals: { learning_comment: @learning_comment }
+      ),
+      turbo_stream.replace(
+        "flash_messages",
+        partial: "shared/flash_messages" # フラッシュメッセージを更新
       )
-    else
-      render turbo_stream: turbo_stream.replace(
-        "learning_comment_#{@learning_comment.id}",
-        partial: "learning_comments/form",
-        locals: { learning_comment: @learning_comment }
-      )
-    end
+    ]
+  else
+    render turbo_stream: turbo_stream.replace(
+      "learning_comment_#{@learning_comment.id}",
+      partial: "learning_comments/form",
+      locals: { learning_comment: @learning_comment }
+    )
   end
+end
 
   private
 
