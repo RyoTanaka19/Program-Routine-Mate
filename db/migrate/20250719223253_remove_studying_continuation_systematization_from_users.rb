a56f88a2cb57a604add0583bf907cea1f660fc9d
ryo88 <ryo98tana@gmail.com>
2025-07-20 07:41:51 +0900

    fix:

--- db/migrate/20250719223253_remove_studying_continuation_systematization_from_users.rb
+++ db/migrate/20250719223253_remove_studying_continuation_systematization_from_users.rb
@@ -1,5 +1,8 @@
+# db/migrate/20250719223253_remove_studying_continuation_systematization_from_users.rb
 class RemoveStudyingContinuationSystematizationFromUsers < ActiveRecord::Migration[7.2]
   def change
-    remove_column :users, :studying_continuation_systematization, :text
+    if column_exists?(:users, :studying_continuation_systematization)
+      remove_column :users, :studying_continuation_systematization, :text
+    end
   end
 end
