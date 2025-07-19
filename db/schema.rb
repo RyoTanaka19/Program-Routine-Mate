7d27a520eecf8463ac33af6e8866cc431378be63
ryo88 <ryo98tana@gmail.com>
2025-07-20 05:48:17 +0900

    学習継続のプロフィール削除

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_07_19_194334) do
+ActiveRecord::Schema[7.2].define(version: 2025_07_19_204353) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -130,7 +130,6 @@ ActiveRecord::Schema[7.2].define(version: 2025_07_19_194334) do
     t.text "self_introduction"
     t.string "provider"
     t.string "uid"
-    t.text "studying_continuation_systematization"
     t.text "withdrawal_reason"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
