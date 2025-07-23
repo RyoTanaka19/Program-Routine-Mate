f519b5e18f51d46bf0d48c865af036a59ade108e
ryo88 <ryo98tana@gmail.com>
2025-07-23 10:23:14 +0900

    fix:リントチエック

--- app/controllers/study_badges_controller.rb
+++ app/controllers/study_badges_controller.rb
@@ -2,7 +2,6 @@ class StudyBadgesController < ApplicationController
   before_action :authenticate_user!
 
   def index
-   # user_study_badgesを先読みしてN+1問題回避
     @study_badges = current_user.study_badges.includes(:user_study_badges)
   end
 end
