b47895426898213427c6f3354ffa6643870f4b24
ryo88 <ryo98tana@gmail.com>
2025-07-23 10:01:08 +0900

    fix:リントチェック

--- app/controllers/study_badges_controller.rb
+++ app/controllers/study_badges_controller.rb
@@ -1,9 +1,8 @@
 class StudyBadgesController < ApplicationController
   before_action :authenticate_user!
 
-def index
-  # user_study_badgesを先読みしてN+1問題回避
-  @study_badges = current_user.study_badges.includes(:user_study_badges)
-end
-
+  def index
+   # user_study_badgesを先読みしてN+1問題回避
+    @study_badges = current_user.study_badges.includes(:user_study_badges)
+  end
 end
