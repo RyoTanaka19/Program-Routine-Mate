3bc8ace8b3e282a01aa7074d3dda5e99194ce57a
ryo88 <ryo98tana@gmail.com>
2025-08-03 23:50:35 +0900

    fix:いいねとコメントLINE通知可能に戻す

--- app/jobs/comment_notification_job.rb
+++ app/jobs/comment_notification_job.rb
@@ -6,11 +6,11 @@ class CommentNotificationJob < ApplicationJob
     study_log = study_comment.study_log
     commenter = study_comment.user
 
-
     safe_content = ActionController::Base.helpers.strip_tags(study_log.content).truncate(30)
     message = "#{commenter.name}さんがあなたの学習記録「#{safe_content}」にコメントしました！"
 
     broadcast_browser_notification(study_log.user.id, message)
+    send_line_notification(study_log.user, message)
   rescue ActiveRecord::RecordNotFound => e
     Rails.logger.warn "CommentNotificationJob failed: #{e.message}"
   end
@@ -19,5 +19,17 @@ class CommentNotificationJob < ApplicationJob
 
   def broadcast_browser_notification(user_id, message)
     ActionCable.server.broadcast("notification_channel_#{user_id}", { message: message })
+  end
+
+  # LINE通知を送信するメソッド
+  def send_line_notification(user, message)
+    return if user.uid.blank?  # ユーザーのLINE IDが無ければ通知しない
+
+    client = LINE_BOT_API  # LINE APIのクライアントインスタンス
+    begin
+      client.push_message(user.uid, { type: "text", text: message })  # LINE通知送信
+    rescue StandardError => e
+      Rails.logger.error("LINE通知送信エラー: #{e.class} #{e.message}\n#{e.backtrace.join("\n")}")
+    end
   end
 end
--- app/jobs/like_notification_job.rb
+++ app/jobs/like_notification_job.rb
@@ -12,6 +12,7 @@ class LikeNotificationJob < ApplicationJob
     message = "#{liker.name}さんがあなたの学習記録「#{safe_content}」にいいねしました！"
 
     broadcast_browser_notification(owner.id, message)
+    send_line_notification(owner, message)
   rescue ActiveRecord::RecordNotFound => e
     Rails.logger.warn "LikeNotificationJob failed: #{e.message}"
   end
@@ -20,5 +21,17 @@ class LikeNotificationJob < ApplicationJob
 
   def broadcast_browser_notification(user_id, message)
     ActionCable.server.broadcast("notification_channel_#{user_id}", { message: message })
+  end
+
+  # LINE通知を送信するメソッド
+  def send_line_notification(user, message)
+    return if user.uid.blank?  # ユーザーのLINE IDが無ければ通知しない
+
+    client = LINE_BOT_API  # LINE APIのクライアントインスタンス
+    begin
+      client.push_message(user.uid, { type: "text", text: message })  # LINE通知送信
+    rescue StandardError => e
+      Rails.logger.error("LINE通知送信エラー: #{e.class} #{e.message}\n#{e.backtrace.join("\n")}")
+    end
   end
 end
